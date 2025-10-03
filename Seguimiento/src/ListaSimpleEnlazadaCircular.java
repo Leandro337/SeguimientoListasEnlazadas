40779bd2d5293e955a8e0b58199058d6b6b23ce5
karenlara1 <karena.larab@uqvirtual.edu.co>
2025-10-02 22:15:35 -0500

    Update ListaSimpleEnlazadaCircular.java
    Creación método OrdenNatural para la listaEnlazadaCircular

--- Seguimiento/src/ListaSimpleEnlazadaCircular.java
+++ Seguimiento/src/ListaSimpleEnlazadaCircular.java
@@ -1,4 +1,4 @@
-public class ListaSimpleEnlazadaCircular<T> {
+public class ListaSimpleEnlazadaCircular<T extends Comparable<T>> {
 
     private Nodo<T> primero;
     private int tam;
@@ -42,6 +42,38 @@ public class ListaSimpleEnlazadaCircular<T> {
             newNodo.setProximo(primero);
         }
 
+        tam++;
+    }
+
+    public void ordenNatural(T dato){
+        Nodo<T> nuevo = new Nodo<>(dato);
+
+        //Si la lista está vacía, el nuevo nodo apunta a sí mismo
+        if(primero == null){
+            primero = nuevo;
+            nuevo.setProximo(primero);
+
+        //Si el nuevo es menor que el primero, se inserta al inicio y el último (antiguo primero) debe actualizar su enlace
+        //y seguir apuntando al primero
+        } else if (dato.compareTo(primero.getDato()) < 0){
+            Nodo<T> ultimo = primero;
+            while (ultimo.getProximo() != primero){
+                ultimo = ultimo.getProximo();
+            }
+            nuevo.setProximo(primero);
+            primero = nuevo;
+            ultimo.setProximo(primero);
+
+        //Ahora se recorre la lista en busca del lugar adecuado.
+        } else {
+            Nodo<T> actual = primero;
+            while(actual.getProximo() != primero && dato.compareTo(actual.getProximo().getDato()) >= 0){
+                actual = actual.getProximo();
+            }
+
+            nuevo.setProximo(actual.getProximo());
+            actual.setProximo(nuevo);
+        }
         tam++;
     }
 }
